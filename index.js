1b55d3b5ab1809f86ecba2418d5b70f7a8131563
rabayarabu <rabu9149@gmail.com>
2023-04-18 13:06:12 +0600

    Fixed eslint error

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const menu = document.querySelector(".menu_mobile");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".close");
-const menuItem = document.querySelectorAll(".menu_link");
+const menu = document.querySelector('.menu_mobile');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.close');
+const menuItem = document.querySelectorAll('.menu_link');
 
-// console.log(menuItem);
-
-hamburger.addEventListener("click", () => {
-    console.log(closeIcon)
-    menu.classList.toggle("active");
-    hamburger.classList.toggle("hide");
-    closeIcon.classList.toggle("show");
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('hide');
+  closeIcon.classList.toggle('show');
 });
 
-closeIcon.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    hamburger.classList.toggle("hide");
-    closeIcon.classList.toggle("show");
+closeIcon.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('hide');
+  closeIcon.classList.toggle('show');
 });
 
 menuItem.forEach((element) => {
-    element.addEventListener("click", () => {
-        menu.classList.toggle("active");
-        hamburger.classList.toggle("hide");
-        closeIcon.classList.toggle("show");
-    });
+  element.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('hide');
+    closeIcon.classList.toggle('show');
+  });
 });
