b2aa862acd93c8c9d8554fb34a885e578bc121fe
rabayarabu <rabu9149@gmail.com>
2023-04-20 02:36:18 +0600

    Add popup window object

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ menuItem.forEach((element) => {
     closeIcon.classList.toggle('show');
   });
 });
+
+const projects = [
+  {
+    name: 'Multi Post Stories',
+    img: 'Images/Img Placeholder.png',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desc1:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+    liveVersion: 'https://rabayarabu.github.io/Portfolio-setup-and-mobile-first/',
+    sourceCode: 'https://github.com/rabayarabu/Portfolio-setup-and-mobile-first.git',
+  }
+];
