f5ff6427297d6bc485933c2ad84a4aa7de5b484f
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-11 16:37:52 +0600

    Use mui Avatar

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+function Navbar () {
+    return (
+        <Avatar alt="Rabu" src="/images/Rabu.jpg" />
+    )
+}
+export default Navbar
