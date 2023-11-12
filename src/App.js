48da554854d9f890af1ebb984ab5e43be8bc149c
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-12 23:51:01 +0600

    Add appbar, container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import ResponsiveAppBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <ResponsiveAppBar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import { AppBar, Container } from '@mui/material';
 
 function Navbar () {
     return (
-        <Avatar alt="Rabu" src="/images/Rabu.jpg" />
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Avatar alt="Rabu" src="/images/Rabu.jpg" />
+            </Container>
+        </AppBar>
+        
     )
 }
 export default Navbar
